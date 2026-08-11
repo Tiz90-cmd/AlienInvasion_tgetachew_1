a68e1a24ca8b6f92ca1805a4f0260a4ed10e8ef5
Tiz90-cmd <tizgetachew90@gmail.com>
2026-08-10 22:09:47 -0400

    buttonand  alieninvasion modified

--- button.py
+++ button.py
@@ -1,6 +1,6 @@
 import pygame.font
 class Button:
-    def _init_(self,ai_game,msg):
+    def __init__(self,ai_game,msg):
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
 
@@ -8,12 +8,12 @@ class Button:
         self.width,self.height =200,50
         self.button_color =(0,135,0)
         self.text_color =(255,255,255)
-        self.front = pygame.front.SysFront(None,48)
+        self.front = pygame.font.SysFont(None,48)
         # Build the button's rectobject and center it.
         self.rect =pygame.Rect(0,0,self.width,self.height)
         self.rect.center = self.screen_rect.center
         # The button message needs to be prepped only once.
-        self._prep_(msg)
+        self._prep_msg(msg)
     def _prep_msg(self,msg):
         """ Turn msg into a renderd image and center text on the button"""
         self.msg_image = self.front.render(msg,True,self.text_color,self.button_color)
